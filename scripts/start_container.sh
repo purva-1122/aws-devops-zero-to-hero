09bc693887fad19ad3346ae00e32ca275beabe3e
purva-1122 <140897459+purva-1122@users.noreply.github.com>
2024-03-29 17:03:35 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull purva123/python-project-app:latest
+docker pull purva123/python-project-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 purva123/python-project-app:latest
+docker run -d -p 5000:5000 purva123/python-project-app
